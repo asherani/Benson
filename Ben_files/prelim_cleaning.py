09a51accf81077273683ee6f4d98e9cc76ed0d3f
Ben-Oren <oren.ben.k@gmail.com>
2018-09-25 01:13:09 -0500

    cleaned up weather csv, cleaning file

--- Ben_files/prelim_cleaning.py
+++ Ben_files/prelim_cleaning.py
@@ -8,11 +8,13 @@ Created on Mon Sep 24 23:23:50 2018
 
 import pandas as pd
 
-#read in data for stations and riders
+#read in data for stations, riders, weather from the main project repo folder
 station_df = pd.read_csv('NYC_Transit_Subway_Entrance_And_Exit_Data.csv')
 rider_df = pd.read_csv('datMTA20180922.csv')
+weather_df = pd.read_csv('weather_data_9-15_9-21.csv', delimiter=';')
 
 #find out whether 'station' variables in the dfs have any values in common
 station_common_values = [station for station in rider_df.STATION if station in station_df['Station Name']]
 #station_common_values = [], so the datasets don't have values for the Station variable in common
 
+    
